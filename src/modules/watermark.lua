acec97ef8ff1b9c0161bddf88f068c56b37402bd
MarchHubOnTopFr <taekarjuna@gmail.com>
2024-11-17 16:38:46 +0700

    Delete src directory

--- src/modules/watermark.lua
+++
@@ -1,8 +0,0 @@
--- modules/watermark.lua
-local Watermark = {}
-
-function Watermark.process(code)
-    return "--[Obfuscated by Hercules v1.6 | discord.gg/Hx6RuYs8Ku (server deleted)]\n" .. code
-end
-
-return Watermark
